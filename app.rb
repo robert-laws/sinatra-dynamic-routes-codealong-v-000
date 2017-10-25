f77038e5a19cb37fef5091f47c7b6163687bfbe5
Robert Laws <roblaws@me.com>
2017-10-25 07:26:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -22,6 +22,7 @@ class App < Sinatra::Base
   get "/multiply/:num1/:num2" do
     number1 = params[:num1]
     number2 = params[:num2]
-    "#{(number1 * number2).to_s}"
+    val = number1 * number2
+    "#{val.to_s}"
   end
 end
